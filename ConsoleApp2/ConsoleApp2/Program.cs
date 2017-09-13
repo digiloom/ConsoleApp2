70f837c7992ca8ca0ca71bc92183fdfd0642f6de
Giff <mark@digital-loom.co.uk>
2017-09-13 13:57:32 +0100

    Default NewFeature01 check-in

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("This is the NewFeature01 branch.");
         }
     }
 }
